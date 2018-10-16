bb0f822bd6206b3474af56ce15043b0324dae9f5
DESKTOP-9GF5CGE\Anja <anjanilsson1992@gmail.com>
2018-10-16 17:42:31 +0200

    Package rename

---
+++ P9/MeteorQuest/src/main/java/com/example/anja/meteorquest/Other/Countdown.java
@@ -0,0 +1,55 @@
+package com.example.anja.meteorquest.Other;
+
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.anja.meteorquest.R;
+
+public class Countdown extends AppCompatActivity {
+
+    TextView txt;
+    int counter = 10;
+    MediaPlayer media;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_countdown);
+
+        txt = (TextView) findViewById(R.id.textView);
+        txt.setText(String.valueOf(counter));
+
+        media = MediaPlayer.create(this, R.raw.batterycharge);
+
+        final Handler handler = new Handler();
+        final int delay = 1000; //milliseconds
+
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                //do something
+                counter --;
+                txt.setText(String.valueOf(counter));
+                playSound();
+
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
+
+    }
+
+    public void playSound(){
+        if (counter == 7){
+            media.start();
+        }
+
+        if (counter == 4){
+            media.start();
+        }
+
+
+
+    }
+}
